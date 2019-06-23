2fb3ed172e439ec5c40d71ff1c8827180613f51e
HeRoMo <hero@asterisk-works.jp>
2019-06-23 16:32:05 +0900

    add popup element

---
+++ popup/components/popup.js
@@ -0,0 +1,20 @@
+export default class PopupElement extends HTMLElement {
+  constructor() {
+    super();
+    const template = document.querySelector('template#popup-tmpl').content.cloneNode(true)
+    const popup = template.querySelector('#popup');
+    popup.addEventListener('click', function() {
+      popup.style.display = 'none';
+    })
+
+    const button = document.createElement('button')
+    button.innerHTML = 'OPEN'
+    button.addEventListener('click', function() {
+      popup.style.display = 'block';
+    });
+
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    shadowRoot.appendChild(template);
+    shadowRoot.appendChild(button);
+  }
+}
